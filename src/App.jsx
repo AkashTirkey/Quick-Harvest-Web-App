9c0c3d49f1969289291c411423fde0cdeb8d8463
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-05-17 20:08:04 +0530

    list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <div className="app-container">
       <Navbar/>
+      <ProductList/>
     </div>
     </>
   )
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import products from '../data/product';
+import ProductCard from './ProductCard';
+import '../styles/ProductList.css';
+
+const categories = [
+    "All",
+    "Vegetables",
+    "Fruits",
+    "Dairy",
+    "Herbs"
+];
+
+const ProductList = () => {
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
+    const filteredProducts = 
+    selectedCategory === "All"
+    ? products
+    : products.filter((item) => item.category === selectedCategory);
+    
+    return (
+        <section className="product-section">
+            <div className="category-pills">
+                {categories.map((category)=>(
+                    <button key={category} className={selectedCategory=== category
+                        ? "pill active-pill"
+                        : "pill"
+                    } onClick={() => setSelectedCategory(category)}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+            
+
+            <div className="product-grid">
+                {filteredProducts.map((item) =>(
+                    <ProductCard
+                        key={item.id}
+                        item = {item}
+                        addToCart={addToCart}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default ProductList;
